63b1d0c70444dd331a054fe7a0cb3741a15770e0
Aryaman Arora <aryaman.arora2020@gmail.com>
2022-12-24 21:22:59 -0800

    arbitrary n-grams

--- draft_2023/lm.py
+++ draft_2023/lm.py
@@ -5,17 +5,22 @@ from utils import lift, fsa_from_samples, estimate_entropy, get_samples
 tokenizer = GPTNeoXTokenizerFast.from_pretrained('EleutherAI/pythia-19m')
 model = GPTNeoXForCausalLM.from_pretrained('EleutherAI/pythia-19m')
 
-def sample_from_lm(tokenizer, model, string, num):
+def sample_from_lm(tokenizer, model, string, num, max_length=30):
     """Generate num samples from a causal LM"""
-
     inputs = tokenizer(string, return_tensors="pt")
-    outputs = model.generate(**inputs, do_sample=True, max_length=30, num_return_sequences=num)
+    outputs = model.generate(**inputs, do_sample=True, max_length=max_length, num_return_sequences=num)
     # print(tokenizer.batch_decode(outputs, skip_special_tokens=True))
     return outputs
+
+def n_gram(seq, n):
+    """Generate n-gram states (e.g. n=2 gives bigrams) from a sequence sampled from an LM"""
+    return [tuple(seq[max(0, i - n + 1):i + 1]) for i, y in enumerate(seq)]
     
-def fsa_from_lm(tokenizer, model, string="I", num=10):
+def fsa_from_lm(tokenizer, model, string="I", num=20, gram=100):
+    """Create an FSA from LM generations"""
     generations = sample_from_lm(tokenizer, model, string, num)
-    return fsa_from_samples([tuple(x) for x in generations.tolist()]) 
+    mod = [tuple(n_gram(x, gram) if gram > 1 else x) for x in generations.tolist()]
+    return fsa_from_samples(mod) 
 
 def main():
     print(estimate_entropy(*fsa_from_lm(tokenizer, model)))
